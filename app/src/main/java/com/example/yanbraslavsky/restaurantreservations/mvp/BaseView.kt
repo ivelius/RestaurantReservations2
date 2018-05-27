7d84a26e1cb8ab7ca0c2a7d108407bbb6d9d09f6
yanbraslavsky-home24 <yan.braslavsky@home24.de>
2018-05-27 10:00:37 +0200

    Added loading overlay

--- app/src/main/java/com/example/yanbraslavsky/restaurantreservations/mvp/BaseView.kt
+++ app/src/main/java/com/example/yanbraslavsky/restaurantreservations/mvp/BaseView.kt
@@ -5,14 +5,16 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.TextView
+import com.example.yanbraslavsky.restaurantreservations.R
 import kotlinx.android.synthetic.main.activity_customer_selection.*
 
 
 abstract class BaseView : AppCompatActivity(), BaseContract.View {
 
     override fun showMessage(message: String) {
-        Snackbar.make(window.decorView, message, Snackbar.LENGTH_LONG).show()
+        Snackbar.make(findAnchorView(), message, Snackbar.LENGTH_LONG).show()
     }
+
 
     override fun showLoading() {
         loading_overlay?.let {
@@ -35,9 +37,11 @@ abstract class BaseView : AppCompatActivity(), BaseContract.View {
     }
 
     override fun showError(errorMessage: String) {
-        val snack = Snackbar.make(window.decorView, errorMessage, Snackbar.LENGTH_LONG)
+        val snack = Snackbar.make(findAnchorView(), errorMessage, Snackbar.LENGTH_LONG)
         val tv = snack.view.findViewById(android.support.design.R.id.snackbar_text) as TextView
         tv.setTextColor(Color.RED)
         snack.show()
     }
+
+    private fun findAnchorView() = findViewById(R.id.constraintLayout) ?: window.decorView.rootView
 }
